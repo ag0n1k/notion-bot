c5cadbb32f61f09522ea5460559173fdff0c5302
ag0n1k <ag0n1k@users.noreply.github.com>
2021-02-07 23:26:42 +0300

    NBT-14: added omdb feature

--- source/parsers/omdb.py
+++ source/parsers/omdb.py
@@ -1,47 +1,10 @@
 import requests
 import logging
-from utils import MetaSingleton
+from clients.omdb import NBotOMDBClient
+from utils import get_omdb_id
 from typing import Dict, List
+
 logger = logging.getLogger(__name__)
-
-
-class NBotIMDB(object, metaclass=MetaSingleton):
-    url = 'http://www.omdbapi.com'
-    content: Dict
-
-    def __init__(self, api_key, timeout=5):
-        self.session = requests.Session()
-        self.key = api_key
-        self.timeout = timeout
-        self.type_map = {
-            'episode': NBotIMDBEpisode,
-            'movie': NBotIMDBMovie,
-            'series': NBotIMDBSeries,
-        }
-
-    def get(self, imdb_id):
-        logger.info("Getting the {}".format(imdb_id))
-        res = self.session.get(self.url,
-                               params=dict(
-                                   apikey=self.key,
-                                   i=imdb_id
-                               ),
-                               timeout=self.timeout)
-        res.raise_for_status()
-        self.content = res.json()
-
-    def parse(self):
-        if not self.content:
-            return
-        item = self.type_map[self.content["Type"]]()
-        item.parse()
-
-    def parse_movie(self):
-        pass
-    def parse_series(self):
-        pass
-    def parse_episode(self):
-        pass
 
 
 class NBotIMDBElement:
@@ -59,13 +22,24 @@ class NBotIMDBElement:
     Country: str
     Awards: str
     Poster: str
-    Ratings: List[Dict[str: str]]
+    Ratings: List[Dict]
     Metascore: str
     imdbRating: str
     imdbVotes: str
     imdbID: str
     Type: str
     Response: str
+    notion_types = dict(
+        Year="select",
+        Rated="select",
+        Released="date",
+        Genre="multi_select",
+        Director="multi_select",
+        Actors="multi_select",
+        Writer="multi_select",
+        Country="multi_select",
+        Type="select",
+    )
 
     def parse(self):
         for i in ["Writer", "Actors", "Director"]:
@@ -74,6 +48,44 @@ class NBotIMDBElement:
     def parse_attr(self, attr):
         attribute = self.__getattribute__(attr)
         self.__setattr__(attr, [i.strip() for i in attribute.split(',')])
+
+
+class NBotOMDBParser:
+    content: Dict
+
+    def __init__(self):
+        self.client = NBotOMDBClient()
+        self.session = requests.Session()
+        self.type_map = {
+            'episode': NBotIMDBEpisode,
+            'movie': NBotIMDBMovie,
+            'series': NBotIMDBSeries,
+        }
+        logger.info("{}".format(self.__dict__))
+
+    def get(self, link) -> (NBotIMDBElement, None):
+        imdb_id = get_omdb_id(link)
+        if not imdb_id:
+            logger.error("No imdb id for link {}".format(link))
+            return None
+        logger.info("Getting the {}".format(imdb_id))
+        res = self.session.get(
+            self.client.url,
+            params=dict(apikey=self.client.key, i=imdb_id),
+            timeout=self.client.timeout
+        )
+        res.raise_for_status()
+        self.content = res.json()
+        return self.parse()
+
+    def parse(self) -> (NBotIMDBElement, None):
+        if not self.content:
+            return None
+        item = self.type_map[self.content["Type"]]()
+        item.__dict__.update(self.content)
+        logger.info(item.__dict__)
+        item.parse()
+        return item
 
 
 class NBotIMDBMovie(NBotIMDBElement):

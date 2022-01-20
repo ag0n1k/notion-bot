0a40b3ad5b4cdeb6ddc033040d29669349215356
Andrey Vazhenin <a.vazhenin@skyeng.ru>
2022-01-20 04:14:28 +0300

    Update tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,21 +1,9 @@
-from nbot.utils import domain, get_omdb_id, MetaSingleton
+from nbot.utils import domain, MetaSingleton
 
 
 def test_domain():
     assert domain("http://youtube.com/aw2asd/") == "youtube.com"
     assert domain("http://www.youtube.com/aw2asd/") == "youtube.com"
-
-
-def test_positive_omdb_id():
-    assert get_omdb_id("https://www.imdb.com/title/tt0993840/") == "tt0993840"
-    assert get_omdb_id("https://www.imdb.com/title/tt6079772/?ref_=nv_sr_srsg_0") == "tt6079772"
-    assert get_omdb_id("https://m.imdb.com/title/tt7888964/") == "tt7888964"
-    assert get_omdb_id("https://m.imdb.com/title/tt1865505/?ref_=fn_al_tt_0") == "tt1865505"
-
-
-def test_negative_omdb_id():
-    assert get_omdb_id("https://www.imdb.com") is None
-    assert get_omdb_id("https://m.imdb.com/") is None
 
 
 class TestMeta(metaclass=MetaSingleton):
